7871cff6ebdfca28b8c3e0c611e923289969f715
Ankit_Regmi <regmiank2002@gmail.com>
2025-05-03 10:17:21 +0545

    chapter 4 added

---
+++ chapter 4/activation_func.py
@@ -0,0 +1,46 @@
+import numpy as np 
+import nnfs 
+from nnfs.datasets import spiral_data
+
+class dense_layer:
+    def __init__(self, inputs_no, neurons_no):
+        self.weights = 0.001*np.random.randn(inputs_no, neurons_no)
+        self.biases = np.zeros((1, neurons_no))
+
+    def forward(self, input_values):
+        self.output = np.dot(input_values, self.weights) + self.biases
+        
+#ReLU activation function, for hidden layers 
+class relu_activation:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+#Softmax activation function, for output layer
+class softmax_softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs- np.max(inputs, axis = 1, keepdims=True))
+        probablities = exp_values / np.sum(exp_values, axis = 1, keepdims=True)
+        self.output = probablities
+
+x, y = spiral_data(samples=100, classes=3)
+
+
+#for first layer 
+layer1 = dense_layer(2,3)
+act1 = relu_activation()
+
+#for second layer 
+layer2 = dense_layer(3,3)
+act2 = softmax_softmax()
+
+#spiral_data --> layer1 --> act1 --> layer2 --> act2
+
+#operation in layer 1
+layer1.forward(x)
+act1.forward(layer1.output)
+
+#operation in layer 2 
+layer2.forward(act1.output)
+act2.forward(layer2.output)
+
+print(act2.output[:5])
